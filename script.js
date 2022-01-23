acbc7be794b55c5fc91715b0e3c415626fe84fa2
Alisile Anusri <73689872+AlisileAnusri@users.noreply.github.com>
2022-01-24 03:05:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function compute()
+{
+    var p = document.getElementById("principal").value;
+    if(p<=0)
+    {
+        alert("Enter a positive number")
+        principal.focus();
+    }
+    if(p>0){
+        var rate= document.getElementById("rate").value;
+        var years=document.getElementById("years").value;
+        var interest=(p*rate*years)/100;
+        var amount=Number(p)+Number(interest);
+        var year= new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML=
+        "If you deposit "+p+"\<br\> at an interest rate of "+rate+"%\<br\>You will receive an amount of "+
+        amount+",\<br\> in the year "+year+"\<br\>"
+
+    }
+
+  
+}
+
+function updaterange()
+{
+    var rateval= document.getElementById("rate").value;
+    document.getElementById("range_val").innerText=rateval+" %";
+}
+
+        
